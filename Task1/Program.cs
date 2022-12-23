51c6a47cf323c3facd3e30073dddd1974472bfab
Ivan <tred1000@mail.ru>
2022-12-23 18:26:32 +0300

    Created Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа. Не использовать строки
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int MidNumber(int result)
+    {
+        int div = result / 10;
+        int rem = div % 10;
+        return rem;
+    }
+
+Console.WriteLine("Введите трёхзначное число");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int MidNumberResult = MidNumber(value);
+Console.WriteLine(MidNumberResult);
